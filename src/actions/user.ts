ee5df243e3c49853eef110232a755d157955723f
Ali Hammoud <alloochi.hammoud@gmail.com>
2024-11-09 22:41:58 +0200

    User action server

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -33,6 +33,8 @@ export const onAuthenticateUser = async () => {
             }
         }
 
+        console.log("user email", user.emailAddresses[0].emailAddress);
+
         const newUser = await client.user.create({
             data: {
                 clerkid: user.id,
@@ -74,7 +76,75 @@ export const onAuthenticateUser = async () => {
         }
 
         return { status: 400 }
-    } catch {
+    } catch (error) {
+        console.log("error", error)
         return { status: 500 }
+    }
+}
+
+export const getNotifications = async () => {
+    try {
+        const user = await currentUser()
+        if (!user) return { status: 404 }
+        const notifications = await client.user.findUnique({
+            where: {
+                clerkid: user.id,
+            },
+            select: {
+                notification: true,
+                _count: {
+                    select: {
+                        notification: true,
+                    },
+                },
+            },
+        })
+
+        if (notifications && notifications.notification.length > 0)
+            return { status: 200, data: notifications }
+        return { status: 404, data: [] }
+    } catch {
+        return { status: 400, data: [] }
+    }
+}
+
+export const searchUsers = async (query: string) => {
+    try {
+        const user = await currentUser()
+
+        if (!user) return { status: 404 }
+
+        const users = await client.user.findMany({
+            where: {
+                OR: [
+                    { firstname: { contains: query } },
+                    { lastname: { contains: query } },
+                    { email: { contains: query } },
+                ],
+                NOT: [
+                    { clerkid: user.id }
+                ]
+            },
+            select: {
+                id: true,
+                subscription: {
+                    select: {
+                        plan: true,
+                    }
+                },
+                firstname: true,
+                lastname: true,
+                image: true,
+                email: true
+            },
+        });
+
+        if (users && users.length > 0) {
+            return { status: 200, data: users }
+        }
+
+        return { status: 404, data: undefined }
+    } catch {
+        return { status: 500, data: undefined }
     }
 }
