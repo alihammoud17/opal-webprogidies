11529fbb832dea8dc73546d266f5880a074dfb29
Ali Hammoud <alloochi.hammoud@gmail.com>
2024-11-15 22:35:19 +0200

    Folder section padding fix

--- src/components/global/folders/folder.tsx
+++ src/components/global/folders/folder.tsx
@@ -26,7 +26,7 @@ const Folder = ({
 
     // wip: Add loading state
     return (
-        <div className={cn('flex hover:bg-neutral-800 cursor-pointer transition duration-150 items-center gap-2 justify-between min-w-[250px] py-6 px-4 rounded-lg border-[1px]')}>
+        <div className={cn('flex hover:bg-neutral-800 cursor-pointer transition duration-150 items-center gap-2 justify-between min-w-[250px] py-4 px-4 rounded-lg border-[1px]')}>
             <Loader state={false}>
                 <div className='flex flex-col gap-[1px]'>
                     <p className='text-neutral-300'>
